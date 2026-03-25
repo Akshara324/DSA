898a5cd0620387bb1bd9b5588de7dcd5c25c34ba
Akshara324 <aksharaishu11@gmail.com>
2026-03-25 20:12:36 +0530

    Add files via upload

---
+++ Cookies.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class Cookies {
+
+    public static int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g); 
+        Arrays.sort(s); 
+
+        int i = 0;
+        int j = 0;
+
+        while (i < g.length && j < s.length) {
+            if (s[j] >= g[i]) {
+                i++; 
+            }
+            j++; 
+        }
+
+        return i; 
+    }
+
+    public static void main(String[] args) {
+        int[] g = {1, 2, 3}; 
+        int[] s = {1, 1};   
+
+        System.out.println("Satisfied children: " + findContentChildren(g, s));
+    }
+}
